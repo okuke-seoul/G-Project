ab2f6f10c3c34d198a7bf602a30d2407fa12c34d
onesup <onesup.lee@gmail.com>
2015-05-10 17:49:17 +0900

    부트스트랩 테마 적용

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,32 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( bootstrap-theme.min.css )
+Rails.application.config.assets.precompile += %w( jquery.vegas.min.css )
+Rails.application.config.assets.precompile += %w( owl.carousel.css )
+Rails.application.config.assets.precompile += %w( owl.theme.css )
+Rails.application.config.assets.precompile += %w( owl.transitions.css )
+Rails.application.config.assets.precompile += %w( animate.css )
+Rails.application.config.assets.precompile += %w( font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( lightbox.css )
+Rails.application.config.assets.precompile += %w( styles.css )
+Rails.application.config.assets.precompile += %w( custom.css )
+Rails.application.config.assets.precompile += %w( font.css )
+
+Rails.application.config.assets.precompile += %w( jquery.min.js )
+Rails.application.config.assets.precompile += %w( jquery.fitvids.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( jquery.easing.1.3.js )
+Rails.application.config.assets.precompile += %w( jquery.vegas.min.js )
+Rails.application.config.assets.precompile += %w( detectmobilebrowser.js )
+Rails.application.config.assets.precompile += %w( jquery.scrollstop.min.js )
+Rails.application.config.assets.precompile += %w( owl.carousel.min.js )
+Rails.application.config.assets.precompile += %w( lightbox.min.js )
+Rails.application.config.assets.precompile += %w( wow.min.js )
+Rails.application.config.assets.precompile += %w( functions.min.js )
+Rails.application.config.assets.precompile += %w( initialise-functions.js )
+
+
+
 # Rails.application.config.assets.precompile += %w( search.js )
